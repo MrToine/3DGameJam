8b91ff9f1f68c4d3808cf233a5c03c64c4d1565c
solene <solenerise@gmail.com>
2025-07-03 09:30:48 +0200

    Added "Text component" to shotgun drop.

--- 3DGameJam/Assets/_/Features/Character/Runtime/Player/Shotgun.cs
+++ 3DGameJam/Assets/_/Features/Character/Runtime/Player/Shotgun.cs
@@ -1,11 +1,24 @@
 using System;
 using Character.Runtime.Player;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Character.Runtime
 {
     public class Shotgun : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI _textComponent;
+        
+        private void Update()
+        {
+            _counter += Time.deltaTime;
+            if (_counter > _despawnTime)
+            {
+                _textComponent.gameObject.SetActive(false);
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
@@ -13,7 +26,17 @@ namespace Character.Runtime
                 var cs = other.GetComponent<ClickShooter>();
                 cs.m_shotgunUnlocked = true;
                 cs._currentWeapon = ClickShooter.WeaponType.Shotgun;
+               DisplayText();
             }
         }
+
+        
+        private void DisplayText()
+        {
+            _textComponent.gameObject.SetActive(true);
+        }
+        
+        private float _counter;
+        [SerializeField] private float _despawnTime;
     }
 }
